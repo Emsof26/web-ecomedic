deb0946c3425ab593eca474035dfab18400e828c
Emsof26 <emilyagrada4@gmail.com>
2026-09-04 04:49:00 +0100

    Se agregó la ruta de Nuevo Informe Ecográfico

--- web-comedic/src/routes/AppRoutes.tsx
+++ web-comedic/src/routes/AppRoutes.tsx
@@ -4,11 +4,10 @@ import {
   Routes,
 } from "react-router-dom";
 
-
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
 import PatientsHistoryPage from "../pages/patients/PatientsHistoryPage";
-
+import NewUltrasoundReportPage from "../pages/reports/NewUltrasoundReportPage";
 
 function AppRoutes() {
   return (
@@ -16,11 +15,11 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/pacientes" element={<PatientsHistoryPage />} />
+        <Route path="/nuevo-informe" element={<NewUltrasoundReportPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default AppRoutes;
